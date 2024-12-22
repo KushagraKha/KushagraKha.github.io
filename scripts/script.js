ad8265f8ef2ff1ee865920ced608b2af52eb1ad3
KushagraKha <khare98@gmail.com>
2024-12-22 17:00:25 +1100

    completed basic framework for index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.main-text');
+    const elements = document.querySelectorAll('.main-text');
 
-    container.addEventListener('mouseover', () => {
-        container.style.backgroundColor = 'white';
-        container.style.color = 'black';
-        container.style.fontSize = '50px';
-        container.style.transition = 'background-color 0.3s, color 0.3s';
-    });
+    elements.forEach(container => {
+        container.addEventListener('mouseover', () => {
+            container.style.backgroundColor = 'white';
+            container.style.color = 'black';
+            container.style.fontSize = '40px';
+            container.style.transition = 'color 0.3s, font-size 0.3s';
+        });
 
-    container.addEventListener('mouseout', () => {
-        container.style.backgroundColor = 'transparent';
-        container.style.fontSize = '40px';
-        container.style.color = 'white';
+        container.addEventListener('mouseout', () => {
+            container.style.backgroundColor = 'transparent';
+            container.style.fontSize = '25px';
+            container.style.color = 'white';
+        });
     });
 });
